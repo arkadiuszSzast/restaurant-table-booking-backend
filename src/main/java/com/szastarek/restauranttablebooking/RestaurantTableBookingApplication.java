2f6733a9873b5378ab79c8a6f8bcb357ff482138
Arkadiusz <szastarek@gmail.com>
2020-02-01 15:00:12 +0100

    Init commit

---
+++ src/main/java/com/szastarek/restauranttablebooking/RestaurantTableBookingApplication.java
@@ -0,0 +1,13 @@
+package com.szastarek.restauranttablebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantTableBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantTableBookingApplication.class, args);
+    }
+
+}
