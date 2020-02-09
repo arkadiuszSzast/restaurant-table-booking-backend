30cd4a71ea10f79d7c7bfd74e9e37d773d0f0bc4
arkadiusz <szastarek@gmail.com>
2020-02-09 11:08:13 +0100

    Enable swagger

--- src/main/java/com/szastarek/restauranttablebooking/RestaurantTableBookingApplication.java
+++ src/main/java/com/szastarek/restauranttablebooking/RestaurantTableBookingApplication.java
@@ -2,9 +2,8 @@ package com.szastarek.restauranttablebooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 
-@SpringBootApplication(exclude = WebMvcAutoConfiguration.class)
+@SpringBootApplication
 public class RestaurantTableBookingApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/szastarek/restauranttablebooking/config/WebFluxConfig.java
+++ src/main/java/com/szastarek/restauranttablebooking/config/WebFluxConfig.java
@@ -3,11 +3,11 @@ package com.szastarek.restauranttablebooking.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.ResourceHandlerRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurationSupport;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
 @EnableWebFlux
-public class WebFluxConfig extends WebFluxConfigurationSupport {
+public class WebFluxConfig implements WebFluxConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
